a1098face1d51b1a66db6aeae7cbb9736ebd0254
Aman Shahi <amanshahidev@gmail.com>
2026-07-01 18:09:06 +0530

    removed the full database cleanup on startup

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -4,7 +4,7 @@ import { Post } from "../models/Post.js";
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connected Successfully ✅");=
+    console.log("MongoDB Connected Successfully ✅");
   } catch (error) {
     console.error("Error while connecting to MongoDB ❌", error.message);
     process.exit(1);
